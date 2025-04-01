9d16e908b69955344f258cb4ce799c57c84cfc86
emil19ro <emil19ro@gmail.com>
2025-04-01 21:50:22 +0200

    using mangodb real database for fetching pets data

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const petTemplate = document.querySelector("#pet-card-template");
 const wrapper = document.createDocumentFragment();
 
-const petsDataUrl = "https://emil19ro.github.io/Pet-Adoption/data/pets.json";
+const petsDataUrl = "https://rainbow-tiramisu-28065d.netlify.app/.netlify/functions/pets";
 async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast");
   const weatherData = await weatherPromise.json();
